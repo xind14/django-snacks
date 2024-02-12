8e576d66868854a92a61dc4c4608cb3eafaf9a19
Xin Deng <xind1470@gmail.com>
2024-02-12 15:43:29 -0800

    added home page content

--- snacks_project/settings.py
+++ snacks_project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party
+    'compressor',
+
     # my apps
     'snacks',
 ]
@@ -126,3 +129,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
